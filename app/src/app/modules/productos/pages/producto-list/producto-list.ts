0ab7ef3c68b388ca2943529a1bd1d3a323fba3e7
gonjgil <gonzalo.javier.gil@gmail.com>
2026-06-28 18:10:27 -0300

    correccion de warnings

--- app/src/app/modules/productos/pages/producto-list/producto-list.ts
+++ app/src/app/modules/productos/pages/producto-list/producto-list.ts
@@ -1,13 +1,15 @@
 import {Component, signal} from '@angular/core';
 import {ProductoService} from '../../../../services/productos/producto-service';
 import {Producto} from '../../interfaces/producto.interface';
-import {CurrencyPipe} from '@angular/common';
+// import {CurrencyPipe} from '@angular/common';
 import {TablaProductos} from '../../components/tabla-productos';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-producto-list',
-  imports: [CurrencyPipe, TablaProductos, RouterLink],
+  imports: [
+    // CurrencyPipe,
+    TablaProductos, RouterLink],
   templateUrl: './producto-list.html',
   styleUrl: './producto-list.css',
 })
