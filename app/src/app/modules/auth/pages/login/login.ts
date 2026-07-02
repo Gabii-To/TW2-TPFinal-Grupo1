b285c6a64559da131f5a85ffcdf6c72199c04973
JoaquinDiaz <joaquindiazantunez15@gmail.com>
2026-07-02 19:42:54 -0300

    Mini cambio a Login (#9)
    En caso de que tus credenciales sean invalidas, la web te lo hace saber

--- app/src/app/modules/auth/pages/login/login.ts
+++ app/src/app/modules/auth/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../../services/auth/auth-service';
@@ -12,16 +12,18 @@ import { AuthService } from '../../../../services/auth/auth-service';
 export class Login {
   loginForm: FormGroup;
   returnUrl!: string;
+  loginError: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private authService: AuthService,
+    private cdr: ChangeDetectorRef
   ) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+      password: ['', [Validators.required]],
     });
   }
 
@@ -30,18 +32,25 @@ export class Login {
   }
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
+    this.loginError = '';
 
-      this.authService.login(email, password).subscribe({
-        next: (response) => {
-          console.log('Login exitoso', response);
-          this.router.navigateByUrl(this.returnUrl || '/');
-        },
-        error: (err) => {
-          console.error('Error de login', err);
-        },
-      });
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
     }
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password).subscribe({
+      next: (response) => {
+        console.log('Login exitoso', response);
+        this.router.navigateByUrl(this.returnUrl || '/');
+      },
+      error: (err) => {
+        console.error('Error de login', err);
+        this.loginError = 'Email o contraseña inválidos';
+        this.cdr.detectChanges();
+      },
+    });
   }
 }
