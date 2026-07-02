b355396106c55ccd6f3923b8b2dcad8c7e11c850
Gabii-To <gabriel.torrico@yahoo.com>
2026-07-02 03:53:17 -0300

    perfil de usuario

--- app-back/src/routes/login-router/login.router.ts
+++ app-back/src/routes/login-router/login.router.ts
@@ -5,8 +5,5 @@ const router = Router();
 const authController = new AuthController();
 
 router.post("/", authController.login.bind(authController));
-router.post("/login", authController.login.bind(authController));
-// router.post("/register", authController.register.bind(authController));
-// router.put("/password", authController.cambiarPassword.bind(authController));
 
 export default router;
